80693b974ec9b12952b486652c4e1de9b94a3a49
Roman <slyusarrv@yandex.ru>
2021-08-02 13:52:36 +0500

    some edits

--- lesson 5 - Operators, filtering, sorting and limiting. Data aggregation/lesson_5_data_aggregation.sql
+++ lesson 5 - Operators, filtering, sorting and limiting. Data aggregation/lesson_5_data_aggregation.sql
@@ -24,6 +24,29 @@ select * from Users;
 
 # 2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. 
 # Следует учесть, что необходимы дни недели текущего года, а не года рождения.
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
  
+ -- SELECT timestampdiff(YEAR, date_of_birth, NOW()) FROM users;
+ 
+ -- В процессе выполнения!!!
+ 
+ select * from users;
 
 # 3. (по желанию) Подсчитайте произведение чисел в столбце таблицы.
