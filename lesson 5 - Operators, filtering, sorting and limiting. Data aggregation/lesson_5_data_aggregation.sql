575ab3c774be7f33fae9c11690fbe18f2dd96c12
Roman <slyusarrv@yandex.ru>
2021-08-02 10:15:51 +0500

    lesson 5 with aggregation

---
+++ lesson 5 - Operators, filtering, sorting and limiting. Data aggregation/lesson_5_data_aggregation.sql
@@ -0,0 +1,27 @@
+USE test;
+
+# 1. Подсчитайте средний возраст пользователей в таблице users.
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+	id BIGINT unsigned NOT NULL auto_increment PRIMARY KEY,
+	name VARCHAR(30),
+	date_of_birth DATE not null
+	);
+
+
+INSERT INTO `Users` 
+	(`name`, `date_of_birth`) 
+VALUES 
+	('Вася', '1987-02-18'), 
+	('Петя', '1988-06-20'), 
+	('Гоша', '1970-01-15'), 
+	('Дарт Вейдер', '2000-08-02');
+
+select * from Users;
+
+# 2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. 
+# Следует учесть, что необходимы дни недели текущего года, а не года рождения.
+ 
+
+# 3. (по желанию) Подсчитайте произведение чисел в столбце таблицы.
