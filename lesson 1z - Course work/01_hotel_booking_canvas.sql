68e7c59671804fdf09a4a46c5acc6bb381426c9b
Roman <slyusarrv@yandex.ru>
2021-08-24 13:37:08 +0500

    Update 01_hotel_booking_canvas.sql

--- lesson 1z - Course work/01_hotel_booking_canvas.sql
+++ lesson 1z - Course work/01_hotel_booking_canvas.sql
@@ -4,7 +4,7 @@ CREATE database hotel_booking;
 
 use hotel_booking;
 
--- **users** - таблица пользователей системы.
+-- Таблица пользователей системы
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
@@ -27,22 +27,88 @@ CREATE TABLE hotels (
 	adress VARCHAR(500) NOT NULL COMMENT 'Адрес объекта размещения' ,
 	hotel_rooms_id BIGINT UNSIGNED NOT NULL COMMENT 'Ключ к таблице hotel_rooms для соотношения номеров с местом размещения'
 	
-	-- FOREIGN KEY (hotel_rooms_id) REFERENCES Users(id),
+ 	-- FOREIGN KEY (hotel_rooms_id) REFERENCES hotel_rooms(id)
+);
+
+-- Таблица для названия комнат, типа одноместный номер, семейныи номер и пр.
+DROP TABLE IF EXISTS rooms;
+CREATE TABLE rooms (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL COMMENT 'Название комнаты для размещения гостей'
 );
 
 
+-- Таблица для соединения отелей и комнат, организация связи многие ко многим
 DROP TABLE IF EXISTS hotel_rooms;
 CREATE TABLE hotel_rooms (
-	hotel_id BIGINT UNSIGNED NOT NULL,
-	room_id BIGINT UNSIGNED NOT NULL,
+	id SERIAL PRIMARY KEY,
+	hotel_id BIGINT UNSIGNED NOT NULL COMMENT 'Ссылка на отель, в котором находится эта комната',
+	room_id BIGINT UNSIGNED NOT NULL COMMENT 'Ссылка на название комнаты',
+	price DECIMAL(10,2) NOT NULL COMMENT 'Цена за место размещения в сутки',
   
-	PRIMARY KEY (hotel_id, room_id),
-    -- FOREIGN KEY (user_id) REFERENCES Users(id),
-    -- FOREIGN KEY (community_id) REFERENCES Communities(id)
+    FOREIGN KEY (hotel_id) REFERENCES hotels(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
 
 );
 
--- **booking** - таблица заявок на бронирование
+-- Таблица, где перечислены удобства в номере
+DROP TABLE IF EXISTS room_fasilities;
+CREATE TABLE room_fasilities (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL COMMENT 'Название удобств в комнате'
+);
 
+-- Таблица для соединения удобств в номере с конкретным номером
+DROP TABLE IF EXISTS hotel_rooms_to_room_fasilities;
+CREATE TABLE hotel_rooms_to_room_fasilities (
+	hotel_room_id BIGINT UNSIGNED NOT NULL,
+	room_fasilities_id BIGINT UNSIGNED NOT NULL,
+  
+	PRIMARY KEY (hotel_room_id, room_fasilities_id),
+    FOREIGN KEY (hotel_room_id) REFERENCES hotel_rooms(id),
+    FOREIGN KEY (room_fasilities_id) REFERENCES room_fasilities(id)
 
--- **reviews** - таблица отзывов о проживании
+);
+
+-- Таблица бронирований
+DROP TABLE IF EXISTS bookings;
+CREATE TABLE bookings (
+	id SERIAL PRIMARY KEY,
+	user_id BIGINT unsigned NOT NULL COMMENT 'Ссылка на пользователя, который забронировал номер',
+	hotel_rooms_id BIGINT unsigned NOT NULL COMMENT 'Ссылка на номер, который забронировал пользователь',
+	check_in_date DATE NOT NULL COMMENT 'Дата регистрации',
+	check_out_date DATE NOT NULL COMMENT 'Дата отъезда',
+	status ENUM('Не подтветжнено', 'Подтверждено', 'Отменено') NOT NULL COMMENT 'Статус бронирования',
+	created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (hotel_rooms_id) REFERENCES hotel_rooms(id)
+);
+
+-- Таблица сообщений между пользователем и администратором объекта размещения
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY,
+	from_user_id BIGINT UNSIGNED NOT NULL,
+	to_user_id BIGINT UNSIGNED NOT NULL,
+	body VARCHAR(1000),
+	created_at DATETIME DEFAULT now(),
+	
+	FOREIGN KEY (from_user_id) REFERENCES users(id),
+	FOREIGN KEY (to_user_id) REFERENCES users(id),
+	
+	CHECK(from_user_id <> to_user_id)
+);
+
+-- Таблица для отзывов пользователях о проживании
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+	id SERIAL PRIMARY KEY,
+	booking_id BIGINT UNSIGNED NOT NULL COMMENT 'Ссылка на бронирование, о котором оставляется отзыв',
+	rating DECIMAL(3,1) NOT NULL COMMENT 'Рейтинг конкретного размещения от 0.0 до 10.0',
+	description VARCHAR(1000) DEFAULT 'Текст отзыва не указан' COMMENT 'Текст отзыва по желанию',
+	
+	FOREIGN KEY (booking_id) REFERENCES bookings(id)
+	
+);
